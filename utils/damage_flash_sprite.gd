cdab91c105ca11c2c801b634d10174058c0b8326
OlliDev <oliver.koenig.bi@gmail.com>
2024-03-29 17:13:50 +0100

    Add crate (something to damage)

---
+++ utils/damage_flash_sprite.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+
+func flash() -> void:
+	var tween := create_tween()
+	tween.tween_property(material, "shader_parameter/flash", 1.0, 0.1)
+	tween.tween_property(material, "shader_parameter/flash", 0.0, 0.2)
