b08d724253681fb094987ddaf23ef47defbc5561
OlliDev <oliver.koenig.bi@gmail.com>
2024-03-30 14:34:59 +0100

    improve flashing sprite

--- utils/damage_flash_sprite.gd
+++ utils/damage_flash_sprite.gd
@@ -1,6 +1,10 @@
 extends Sprite2D
 
+var tween: Tween
+
 func flash() -> void:
-	var tween := create_tween()
+	if tween and tween.is_valid():
+		tween.kill()
+	tween = create_tween()
 	tween.tween_property(material, "shader_parameter/flash", 0.5, 0.1)
 	tween.tween_property(material, "shader_parameter/flash", 0.0, 0.2)
