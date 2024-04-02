84b4d6b49a5329780124e35b841f804861725dcc
OlliDev <oliver.koenig.bi@gmail.com>
2024-04-02 15:38:10 +0200

    Make option for damage direction to be centered

--- components/fighting/damage_dealer.gd
+++ components/fighting/damage_dealer.gd
@@ -1,10 +1,14 @@
 class_name DamageDealer
 extends Node2D
 
+signal dealed_damage(receiver: DamageReceiver)
+
 @export var damage: int = 10
 @export var directional_impact: bool = true
 # Increases knockback and stuff
 @export var impact_velocity_magnitude: float = 10
+# Influences how knockback works
+@export var force_away: bool = false
 @export var knockback: KnockbackData
 # Wont deal damage if dead
 @export var killable: Killable
@@ -16,7 +20,10 @@ func apply_damage(node: Node) -> bool:
 
 	var dmg := Components.get_damage_receiver(node)
 	if dmg and !dmg.is_invincible():
+		if force_away and node is Node2D:
+			global_transform = global_transform.looking_at(node.global_position)
 		dmg.take_damage(damage, self)
+		dealed_damage.emit(damage)
 		return true
 	
 	return false
